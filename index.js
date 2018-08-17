6440642b72554fda3510d6159ee37b62093c099d
Gejid <42443177+Gejid@users.noreply.github.com>
2018-08-17 10:43:01 +0300

    Description
    Added the comments to both bot scripts and the config.

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+// Requires and necessary variables
+// Discord api
 const discord = require("discord.js")
+// The client of the bot
 const client = new discord.Client()
+// FileSystem api
 const fs = require("fs")
+// Getting the token
 const token = fs.readFileSync("token", "utf8")
+// And the config
+const config = require("./config")
 
+// Logining into the Discord by client
 client.login(token)
 
+// Event that shows a message to the specific channel when client is ready
 client.on("ready", () => {
+    // Channel id
     var channelnew = "478832860036464650"
 
+    // Client sends message
     client.channels.get(channelnew).send("Я вернулся из Орла, ла-ла, ла-ла.")
     
 })
+
+// Event that catches if someone sent message
+client.on("message", async message => {
+    // Check if the message author isn't the bot
+    if(message.author.bot) return
+    // Check if the message has prefix at the start
+    if(message.content.indexOf(config.prefix) != 0) return
+
+})
+
